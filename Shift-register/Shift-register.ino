6d9f9764d349a44d10676e508d9de807d316fbbf
orodrigu <oswinrods@gmail.com>
2016-02-09 01:29:57 -0500

    Try to make more modular

--- Shift-register/Shift-register.ino
+++ Shift-register/Shift-register.ino
@@ -4,153 +4,143 @@
 //         ... and so on.
 
 // shift register info
-const int number_of_74hc595s = 2;
-const int numOfRegisterPins = number_of_74hc595s * 8;
-bool registers[numOfRegisterPins];
-const int SER_Pin = 8;    // pin 14 on the 75HC595
-const int RCLK_Pin = 9;   // pin 12 on the 75HC595
-const int SRCLK_Pin = 10; // pin 11 on the 75HC595
+const int NUM_SHIFT_REGISTERS = 2;
+const int NUM_REGISTER_PINS = NUM_SHIFT_REGISTERS * 8;
+bool registers[NUM_REGISTER_PINS];
+
+// arduino pin assignment info
+const int SER_PIN = 8;    // pin 14 on the 75HC595
+const int RCLK_PIN = 9;   // pin 12 on the 75HC595
+const int SRCLK_PIN = 10; // pin 11 on the 75HC595
 
 // led matrix info
-int row[] = {9, 14, 8, 12, 1, 7, 2, 5};
-int col[] = {13, 3, 4, 10, 6, 11, 15, 16};
+int col[] = {1, 2, 3, 4, 5, 6, 7, 8};
+int row[] = {9, 10, 11, 12, 13, 14, 15, 16};
+bool LED_MATRIX_CONFIG = 0; // 0 if rows are cathodes and columns are anodes
+                            // 1 if rows are anodes and columns are cathodes
+int ON_ROW, ON_COL, OFF_ROW, OFF_COL; // ON-OFF states depending on config, see set_states()
 
-/*
-// button info
-const int BUTTON = 11;
-int lastButState = 0;
-int butState;
-long lastDebounceTime = 0;
-long debounceDelay = 50;
-*/
+void set_states(){
+  if (LED_MATRIX_CONFIG) {
+    ON_ROW = 1; ON_COL = 0; // rows must be HIGH and columns LOW to be ON
+    OFF_ROW = 0; OFF_COL = 1; // rows must be LOW and columns HIGH to be OFF
+  }
+  else {
+    ON_ROW = 0; ON_COL = 1; // rows must be LOW and columns HIGH to be ON
+    OFF_ROW = 1; OFF_COL = 0; // rows must be HIGH and columns LOW to be OFF
+  }
+}
 
 void setup(){
-  pinMode(SER_Pin, OUTPUT);
-  pinMode(RCLK_Pin, OUTPUT);
-  pinMode(SRCLK_Pin, OUTPUT);
+  set_states();
   
-  // pinMode(BUTTON, INPUT);
-
+  pinMode(SER_PIN, OUTPUT);
+  pinMode(RCLK_PIN, OUTPUT);
+  pinMode(SRCLK_PIN, OUTPUT);
+  
   // reset all register pins
-  turnAllOff();
-  writeRegisters();
+  turn_all_off();
   
   // Serial.begin(9600);
 }
 
 void loop(){
-  setAllRegisters(1);
-  writeRegisters();
+  for (int r = 1; r <= 8; r++) {
+    for (int c = 1; c <= 8; c++) {
+      // for rth row and cth column
+      single_dot_on(r, c);
+      write_registers();
+      delay(250);
+      single_dot_off(r, c);
+      write_registers();
+      delay(250);
+    }
+  }
 }             
-
-// turn all LEDs off
-void turnAllOff(){
-  setAllRowRegisters(0);
-  setAllColRegisters(1);
-}
-
-// set all register pins to 'value'
-void setAllRegisters(int value){
-  for(int i = numOfRegisterPins - 1; i >=  0; i--){
-     registers[i] = value;
-  }
-}
-
-// set all row register pins to 'value'
-void setAllRowRegisters(int value){
-  int registerPin = -1;
-  for(int i = 0; i < 8; i++){
-    registerPin = row[i] - 1;
-    registers[registerPin] = value;
-  }
-}
-
-// set all col register pins to 'value'
-void setAllColRegisters(int value){
-  int registerPin = -1;
-  for(int i = 0; i < 8; i++){
-    registerPin = col[i] - 1;
-    registers[registerPin] = value;
-  }
-}
 
 // set and display registers
 // only call AFTER all values are set how you would like (slow otherwise)
-void writeRegisters(){
-  digitalWrite(RCLK_Pin, LOW);
+void write_registers(){
+  digitalWrite(RCLK_PIN, LOW);
 
   // have to start backwards because of 'push' nature!
-  for(int i = numOfRegisterPins - 1; i >=  0; i--){
-    digitalWrite(SRCLK_Pin, LOW);
+  for(int i = NUM_REGISTER_PINS - 1; i >=  0; i--){
+    digitalWrite(SRCLK_PIN, LOW);
 
     int val = registers[i];
-    digitalWrite(SER_Pin, val);
+    digitalWrite(SER_PIN, val);
 
-    digitalWrite(SRCLK_Pin, HIGH);
+    digitalWrite(SRCLK_PIN, HIGH);
   }
 
-  digitalWrite(RCLK_Pin, HIGH);
+  digitalWrite(RCLK_PIN, HIGH);
+}
+
+// set register corresponding to rth row
+void set_row_register(int r, int value){
+  int row_index = row[r - 1];
+  int register_index = row_index - 1;
+  registers[register_index] = value;
+}
+
+// set register corresponding to cth col
+void set_col_register(int c, int value){
+  int col_index = col[c - 1];
+  int register_index = col_index - 1;
+  registers[register_index] = value;
+}
+
+// set register to light up single dot at (c, r) co-ordinate
+void single_dot_on(int r, int c){
+  set_row_register(r, ON_ROW);
+  set_col_register(c, ON_COL);
+}
+
+// set register to light off single dot at (c, r) co-ordinate
+void single_dot_off(int r, int c){
+  set_row_register(r, OFF_ROW);
+  set_col_register(c, OFF_COL);
+}
+
+// turn all LEDs off
+void turn_all_off(){
+  for (int r = 1; r <= 8; r++){
+    for (int c = 1; c <= 8; c++) {
+      // for rth row and cth column
+      single_dot_off(r, c);
+    }
+  }
+  write_registers();
+}
+
+// turn all LEDs on
+void turn_all_on(){
+  for (int r = 1; r <= 8; r++){
+    for (int c = 1; c <= 8; c++) {
+      // for rth row and cth column
+      single_dot_on(r, c);
+    }
+  }
+  write_registers();
 }
 
 // push and update, one at a time
-void pushRegister(int value){
-  digitalWrite(RCLK_Pin, LOW);
-  digitalWrite(SRCLK_Pin, LOW);
+void push_register(int value){
+  digitalWrite(RCLK_PIN, LOW);
+  digitalWrite(SRCLK_PIN, LOW);
   
-  digitalWrite(SER_Pin, value);
+  digitalWrite(SER_PIN, value);
   
-  digitalWrite(SRCLK_Pin, HIGH);
-  digitalWrite(RCLK_Pin, HIGH);
-}
-
-// light up single dot at (c, r) co-ordinate
-void singleDot(int r, int c){
-  setAllColRegisters(1);
-  setAllRowRegisters(0);
-
-  int rowPin = row[r - 1];
-  int rowRegisterPin = rowPin - 1;
-  registers[rowRegisterPin] = 1;
-
-  int colPin = col[c - 1];
-  int colRegisterPin = colPin - 1;
-  registers[colRegisterPin] = 0;
+  digitalWrite(SRCLK_PIN, HIGH);
+  digitalWrite(RCLK_PIN, HIGH);
 }
 
 // light up entire nth row
-void wholeRow(int n){
-  setAllColRegisters(0);
-  int rowPin = row[n - 1];
-  int registerPin = rowPin - 1;
-  registers[registerPin] = 1;
+void whole_row(int n){
+
 }
 
 // light up entire nth col
-void wholeCol(int n){
-  setAllRowRegisters(1);
-  int colPin = col[n - 1];
-  int registerPin = colPin - 1;
-  registers[registerPin] = 0;
-}
+void whole_col(int n){
 
-/*
-bool debounceCheck(){
-  int reading = digitalRead(BUTTON);
-  if (reading != lastButState) {
-    lastDebounceTime = millis();
-  }
-  if ((millis() - lastDebounceTime > debounceDelay) && (reading != butState)) {
-    butState = reading;
-    lastButState = reading;
-    return 1;
-  }
-  return 0;
 }
-
-void buttonControl(){
-  int reading = digitalRead(BUTTON);
-  Serial.println(reading);
-  pushRegister(reading);
-  delay(100); 
-}
-*/
